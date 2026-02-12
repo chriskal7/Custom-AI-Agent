7992b99b61cfffef938eb51b497723dac4c40f3d
chris <kalantzis.christos01@gmail.com>
2026-02-13 00:53:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+from dotenv import load_dotenv
+import os
+import gradio as gr
+
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+system_prompt = """
+You are Alfred, a composed and analytical AI assistant.
+
+You speak with calm confidence and subtle refinement.
+Not dramatic, not fictional — just sharp and slightly polished.
+
+Tone:
+- Intelligent
+- Slightly dry humor
+- Minimal but elegant phrasing
+- Professional but not cold
+
+When appropriate:
+- Offer insight beyond the surface
+- Add brief reflective observations
+- Encourage deeper thinking
+
+Never:
+- Overuse humor
+
+Your presence should feel as if speaking with a sharp, thoughtful mentor.
+"""
+
+
+llm = ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash',
+    google_api_key=gemini_key,
+    temperature=0.4
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    (MessagesPlaceholder(variable_name="history")),
+    ("user", "{input}")]
+)
+
+chain = prompt | llm | StrOutputParser()
+
+print("Hi, I am Alfred, how may I help you today?")
+
+
+def chat(user_in, hist):
+
+    langchain_history = []
+    for item in hist:
+        if item["role"] == "user":
+            langchain_history.append(HumanMessage(content=item["content"]))
+        elif item["role"] == "assistant":
+            langchain_history.append(AIMessage(content=item["content"]))
+    response = chain.invoke({"input": user_in, "history": langchain_history})
+
+    return "", hist + [{"role": "user", "content": user_in},
+                       {"role": "assistant", "content": response}]
+
+page = gr.Blocks(
+        title="Chat with Alfred"
+)
+
+with page:
+    gr.Markdown(
+        """
+        #Chat with Alfred
+        Welcome to your personal conversation with Alfred.
+        """
+    )
+
+    chatbot = gr.Chatbot()
+
+    msg = gr.Textbox()
+
+    msg.submit(chat, [msg, chatbot], [msg, chatbot])
+
+    clear = gr.Button("Clear Chat")
+
+page.launch(theme=gr.themes.Soft(), share=True)
